47abd6e4d26992c9e39ded68068fee105c726160
Henry Kaufman <hkaufman19@gmail.com>
2018-01-07 14:35:45 -0700

    aktiv

--- components/Variables.js
+++ components/Variables.js
@@ -1,4 +1,4 @@
-export const fontFamily = 'usual, sans-serif';
+export const fontFamily = 'aktiv grotesk, sans-serif';
 
 export const colors = {
   black: '#111',
--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <HeadContent />
-        <body className="tk-usual">
+        <body className="tk-aktiv-grotesk">
           <Main />
           <NextScript />
         </body>
