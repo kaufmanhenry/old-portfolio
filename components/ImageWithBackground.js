d7b48858ab2b885e82de689c702e6e3632e6757c
Henry Kaufman <hkaufman19@gmail.com>
2019-01-01 19:55:07 -0700

    Add: Unique ID generation so we can have zooming on the screenshots

--- components/ImageWithBackground.js
+++ components/ImageWithBackground.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { colors } from './Variables';
+
+let lastId = 0;
+
+const genUniqueId = (prefix = 'id') => {
+  lastId++;
+  return `${prefix}${lastId}`;
+};
 
 const StyledImageWithBackground = styled.div`
   background-color: #f8f9fa;
@@ -10,6 +17,8 @@ const StyledImageWithBackground = styled.div`
     border-radius: 8px;
     box-shadow: 0 5px 20px rgba(0, 0, 0, .05);
     width: 100%;
+    transition: transform 0.25s ease;
+    cursor: zoom-in;
   }
   p {
     color: ${colors.gray};
@@ -17,18 +26,38 @@ const StyledImageWithBackground = styled.div`
     margin-top: 16px;
     text-align: center;
   }
+  input[type=checkbox] {
+    display: none;
+  }
+  input[type=checkbox]:checked ~ label > img {
+    transform: scale(2);
+    cursor: zoom-out;
+  }
 `;
 
-const ImageWithBackground = ({ imageUrl, description }) => (
-  <StyledImageWithBackground>
-    <img src={imageUrl} alt="img with background" />
-    <p>{description}</p>
-  </StyledImageWithBackground>
-);
+class ImageWithBackground extends Component {
+  static propTypes = {
+    imageUrl: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  }
 
-ImageWithBackground.propTypes = {
-  imageUrl: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
-};
+  componentWillMount = () => {
+    this.uniqueId = genUniqueId();
+  }
+
+  render = () => {
+    const { imageUrl, description } = this.props;
+    console.log(this.uniqueId);
+    return (
+      <StyledImageWithBackground>
+        <input type="checkbox" id={this.uniqueId} />
+        <label htmlFor={this.uniqueId}>
+          <img src={imageUrl} alt="img with background" />
+        </label>
+        <p>{description}</p>
+      </StyledImageWithBackground>
+    );
+  }
+}
 
 export default ImageWithBackground;
