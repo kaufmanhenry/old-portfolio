9e953467b34990dd18dbcea7151accc8f6b79652
Henry Kaufman <hkaufman19@gmail.com>
2019-02-19 08:29:58 -0500

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "54edde3ddb6a6dcc4547";
+/******/ 	var hotCurrentHash = "2634623566e32575ae1c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
